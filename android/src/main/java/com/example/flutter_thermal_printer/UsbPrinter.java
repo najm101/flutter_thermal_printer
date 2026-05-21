24709799f61b33c400b32d4c5d9ac2c260de1233
najm101 <bodinegem@icloud.com>
2026-05-21 23:42:12 +0300

    fix

--- android/src/main/java/com/example/flutter_thermal_printer/UsbPrinter.java
+++ android/src/main/java/com/example/flutter_thermal_printer/UsbPrinter.java
@@ -24,6 +24,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
